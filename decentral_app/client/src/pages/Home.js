c39e609276751f8bd10156529eaeae1808b4fc00
AMSLau <amsl0503@gmail.com>
2019-04-29 11:56:22 +0800

    content

--- decentral_app/client/src/pages/Home.js
+++ decentral_app/client/src/pages/Home.js
@@ -5,12 +5,22 @@ class Home extends Component {
     return(
       <div className="Home">
         <Header/>
-        <h1>Home</h1>
-        <h1>Hello, Welcome to Agora!</h1>
-        <img src = "https://res.cloudinary.com/fyp18003/image/upload/v1556504433/Other/Group.png"/>
+        <div id="Hcontent">
+        <div id="content">
+        <h1>This is Agora, the digital artwork sharing platform that you can upload any artwork via your MetaMask Wallet</h1>
+        Your upload history would be stored in the blockchain which secure the proof to your artwork.
+        </div>
+        <section>
         <img src = "https://res.cloudinary.com/fyp18003/image/upload/v1556504433/Other/Group_2.png"/>
+        </section>
+        </div>
       </div>
     );
   }
 }
 export default Home;
+// <section>
+//
+// <img id="flow" src = "https://res.cloudinary.com/fyp18003/image/upload/v1556504433/Other/Group.png"/>
+// One Wallet,
+// </section>
